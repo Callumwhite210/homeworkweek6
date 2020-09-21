1f578b6417c3fc362aee62e7e587fb093d49079b
Callum White <callumwhite210@gmail.com>
2020-09-21 15:17:01 +0930

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=c21d845b56ef20be1ee5c6b2cf0c8efe";
+
+
+//add searchbar class into queryURL to search for specified city
